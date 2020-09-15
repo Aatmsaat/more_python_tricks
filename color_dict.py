7e8ed685ff9668c8f9bf76cb6aabf7c4c5365987
Vaibhav Gupta <47090589+Aatmsaat@users.noreply.github.com>
2020-09-15 12:37:39 +0530

    Create color_dict.py
    Ascii colors for printing

---
+++ color_dict.py
@@ -0,0 +1,13 @@
+COLOR = {
+    'blue': '\033[94m',
+    'default': '\033[99m',
+    'grey': '\033[90m',
+    'yellow': '\033[93m',
+    'black': '\033[90m',
+    'cyan': '\033[96m',
+    'green': '\033[92m',
+    'magenta': '\033[95m',
+    'white': '\033[97m',
+    'red': '\033[91m'
+}
+print(red+'Hello Everyone')
